737b1a6920d34652cb1e33cd6c2e50f07207651b
李龙 <lilong2576@hanweb.com>
2024-12-09 18:15:01 +0800

    feat: 登录界面使用栅格与表单组件

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,94 @@
+import { Row,Col,Card,Button, Checkbox, Form, Input  } from 'antd';
 import React from 'react';
-
+import {useNavigate} from 'react-router-dom';
 export default function Login() {
+  const navigate = useNavigate();
+  const onFinish = (values) => {
+    console.log('Success:', values);
+    navigate('/admin/student_type')
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
   return (
-    <div>登录界面</div>
+    <>
+    <div className='login-container'>
+      <Row>
+          <Col 
+          md={{
+            span: 8,
+            push: 8
+          }}
+          xs={{
+            span: 22,
+            push: 1
+          }}
+          >
+
+
+          <Card
+            title={<div style={{textAlign:"center","fontSize": 20}}>木叶村教学管理系统</div>}
+            bordered={false}
+            style={{
+              marginTop: '200px'
+            }}
+          >
+            <Form
+                name="basic"
+                labelCol={{
+                  span: 6,
+                }}
+                wrapperCol={{
+                  span: 16,
+                }}
+              
+                initialValues={{
+                  remember: true,
+                }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+              >
+                <Form.Item
+                  label="Username"
+                  name="username"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input your username!',
+                    },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+
+                <Form.Item
+                  label="Password"
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: 'Please input your password!',
+                    },
+                  ]}
+                >
+                  <Input.Password />
+                </Form.Item>
+
+                <Form.Item name="remember" valuePropName="checked" label={null}>
+                  <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+
+                <Form.Item label={null}>
+                  <Button type="primary" htmlType="submit">
+                    Submit
+                  </Button>
+                </Form.Item>
+              </Form>
+          </Card>
+          </Col>
+        </Row>
+    </div>
+    </>
   )
 }
