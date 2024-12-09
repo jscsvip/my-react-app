fb477e4c9a1c9df531743fe08d513f247e70db3c
李龙 <lilong2576@hanweb.com>
2024-12-09 11:52:46 +0800

    feat: 基本配置与引入antd布局组件

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ConfigProvider } from 'antd';
+import { BrowserRouter } from 'react-router-dom';
+import 'antd/dist/reset.css'
+import zhCN from 'antd/locale/zh_CN';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <ConfigProvider locale={zhCN} theme={{ token: { colorPrimary: '#00b96b' } }}>
+      <App />
+    </ConfigProvider>
+  </BrowserRouter>
+
 )
